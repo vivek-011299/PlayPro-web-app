d490589f367a65dcde57958365b5a698f9d65084
vivek-011299 <vivek011299@gmail.com>
2024-05-22 20:28:17 +0530

    backend running

--- backend/src/main/java/com/backend/playpro/PlayproApplication.java
+++ backend/src/main/java/com/backend/playpro/PlayproApplication.java
@@ -8,6 +8,7 @@ public class PlayproApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlayproApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
